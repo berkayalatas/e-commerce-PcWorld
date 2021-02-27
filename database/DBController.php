77450893d5ee4785481c4d30f92c01bcb78ae486
Berkay Alatas <berkay.3304@gmail.com>
2021-02-27 21:47:14 +0300

    Remote DB connection

--- database/DBController.php
+++ database/DBController.php
@@ -1,40 +1,51 @@
 <?php
 
-    class DBController {
-        //database connection 
-        protected $host = 'localhost';
-        protected $user = 'root';
-        protected $password = '';
-        protected $database='pcworld';
+class DBController
+{
+    //development database connection 
+    // protected $host = 'localhost';
+    // protected $user = 'root';
+    // protected $password = '';
+    // protected $database = 'pcworld';
 
-        //connection property
-        public $connection = null;
 
-        //call constructor
-        public function __construct() {
-         $this->connection = mysqli_connect(
+    //remote database connection 
+    protected $host = 'remotemysql.com';
+    protected $user = '2ojATkYoIs';
+    protected $password = 'l16SA78JhG';
+    protected $database = '2ojATkYoIs';
+
+
+    //connection property
+    public $connection = null;
+
+    //call constructor
+    public function __construct()
+    {
+        $this->connection = mysqli_connect(
             $this->host,
             $this->user,
-            $this->password, 
-            $this->database);  
-        
-        if($this->connection->connect_error){
+            $this->password,
+            $this->database
+        );
+
+        if ($this->connection->connect_error) {
             echo "Connection error!! ";
-        } 
-       } 
-
-       //close db, script is stopped or exited.
-       public function __destruct() {
-           $this->closeConnection();
-       }
-
-       //close DB connetion
-       protected function closeConnection() {
-           if($this->connection != null){
-               $this->connection->close();
-               $this->connection = null;
-           }
-       }
+        }
     }
 
- 
+    //close db, script is stopped or exited.
+    public function __destruct()
+    {
+        $this->closeConnection();
+    }
+
+    //close DB connetion
+    protected function closeConnection()
+    {
+        if ($this->connection != null) {
+            $this->connection->close();
+            $this->connection = null;
+        }
+    }
+}
